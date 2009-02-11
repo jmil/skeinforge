36c4cdba60258217780dec2c2ef4d67389e5b5a1
kintel <kintel@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2009-02-11 23:43:36 +0000

    reverted recent changes
    git-svn-id: https://reprap.svn.sourceforge.net/svnroot/reprap/trunk/reprap/miscellaneous/python-beanshell-scripts@2537 cb376a5e-1013-0410-a455-b6b1f9ac8223

--- runskeinforge.sh
+++ runskeinforge.sh
@@ -1,26 +1,26 @@
-  1 #!/bin/sh
-    2 
-    3 #
-    4 # Utility script for keeping track of which preference settings a file was processed
-    5 # with, by copying the current preferences to a date-tagged directory together
-    6 # with the output files.
-    7 #
-    8 # Usage: runskeinforge.sh <model.gts>
-    9 #
-   10 
-   11 dir=`dirname $1`
-   12 file=`basename $1`
-   13 
-   14 for s in .gts .GTS .stl .STL; do
-   15   if [ ! `basename $file $s` = $file ]; then suffix=$s; fi
-   16 done
-   17 
-   18 if [ -n $suffix ]; then
-   19   filename=`basename $file $suffix`
-   20   newdir=$filename-`date +%m%d%H%M`
-   21   mkdir -p $newdir/skeinforge-prefs
-   22   cp $1 $newdir
-   23   cp ~/.skeinforge/*.csv $newdir/skeinforge-prefs
-   24   python skeinforge.py $newdir/$filename$suffix
-   25   echo $PWD/$newdir/${filename}_export.gcode
-   26 fi
+#!/bin/sh
+
+#
+# Utility script for keeping track of which preference settings a file was processed
+# with, by copying the current preferences to a date-tagged directory together
+# with the output files.
+#
+# Usage: runskeinforge.sh <model.gts>
+#
+
+dir=`dirname $1`
+file=`basename $1`
+
+for s in .gts .GTS .stl .STL; do
+  if [ ! `basename $file $s` = $file ]; then suffix=$s; fi
+done
+
+if [ -n $suffix ]; then
+  filename=`basename $file $suffix`
+  newdir=$filename-`date +%m%d%H%M`
+  mkdir -p $newdir/skeinforge-prefs
+  cp $1 $newdir
+  cp ~/.skeinforge/*.csv $newdir/skeinforge-prefs
+  python skeinforge.py $newdir/$filename$suffix
+  echo $PWD/$newdir/${filename}_export.gcode
+fi
